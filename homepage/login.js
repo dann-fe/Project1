cd33f56f79afb4ff05ba7efea52000d5ce91b21a
Daniil Fedotov <dan.wf312@gmail.com>
2024-07-06 19:06:35 +0200

    finished the signup window, new scrollbar added

--- homepage/login.js
+++ homepage/login.js
@@ -1,13 +1,31 @@
-let loginButton = document.querySelector(".login");
-let loginWindow = document.querySelector("#login-popout-window");
-let closeButton = document.querySelector(".close-window")
+const loginButton = document.querySelector(".login");
+const loginWindow = document.querySelector("#login-window");
+const closeButton = document.querySelectorAll(".close-window")
+const body = document.body;
+const joinButton = document.querySelector(".join-button");
+const signupWindow = document.querySelector("#signup-window");
+const modalPopoutWindow = document.querySelector(".modal-popout-window");
 function openLoginWindow () {
     loginWindow.style.display = "flex";
+    body.classList.add("no-scroll");
 }
 
 function closeLoginWindow () {
     loginWindow.style.display = "none";
+    body.classList.remove("no-scroll");
 }
 
+function closeSignupWindow () {
+    signupWindow.style.display = "none";
+    body.classList.remove("no-scroll");
+}
+
+function openSignupWindow (){
+    signupWindow.style.display = "flex";
+ }
+
 loginButton.addEventListener("click", openLoginWindow);
-closeButton.addEventListener("click", closeLoginWindow);
+closeButton[0].addEventListener("click", closeLoginWindow);
+closeButton[1].addEventListener("click", closeSignupWindow);
+// body.addEventListener("click", closeLoginWindow);
+joinButton.addEventListener("click", openSignupWindow);
